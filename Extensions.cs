77571c1c46f987439c2a6a70d4ce6b6621ead063
KvaziKote <hatab200@gmail.com>
2023-06-23 15:15:55 +0300

    Initial commit

---
+++ Extensions.cs
@@ -0,0 +1,168 @@
+﻿using OpenQA.Selenium;
+using System.Collections.ObjectModel;
+
+namespace SeleniumTools
+{
+    public static class WebDriverExtensions
+    {
+        public static bool IsExist(this IWebElement element, By by)
+        {
+            try
+            {
+                element.FindElement(by);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static bool IsExist(this IWebDriver element, By by)
+        {
+            try
+            {
+                element.FindElement(by);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static void CustomeClick(this IWebElement element, IWebDriver driver)
+        {
+            var js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].click();", element);
+        }
+
+        public static IWebElement FindElement(this IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            IWebElement result = null;
+            var durations = 0;
+
+            while (result == null)
+            {
+                try
+                {
+                    result = driver.FindElement(by);
+                    break;
+                }
+                catch (NoSuchElementException)
+                {
+                    durations += 100;
+                    Thread.Sleep(100);
+                }
+
+                if (durations > timeoutInSeconds * 1000)
+                    throw new NoSuchElementException();
+            }
+
+            return result;
+        }
+
+        public static IWebElement FindElement(this IWebElement webElement, By by, int timeoutInSeconds)
+        {
+            IWebElement result = null;
+            var durations = 0;
+
+            while (result == null)
+            {
+                try
+                {
+                    result = webElement.FindElement(by);
+                    break;
+                }
+                catch (NoSuchElementException)
+                {
+                    durations += 100;
+                    Thread.Sleep(100);
+                }
+                catch (StaleElementReferenceException)
+                {
+                    durations += 100;
+                    Thread.Sleep(100);
+                }
+
+                if (durations > timeoutInSeconds * 1000)
+                    throw new NoSuchElementException();
+            }
+
+            return result;
+        }
+
+        public static ReadOnlyCollection<IWebElement> FindElements(this IWebElement driver, By by, int timeoutInSeconds)
+        {
+            var durations = 0;
+
+            while (true)
+            {
+                try
+                {
+                    driver.FindElement(by);
+                    break;
+                }
+                catch (NoSuchElementException)
+                {
+                    durations += 100;
+                    Thread.Sleep(100);
+                }
+
+                if (durations > timeoutInSeconds * 1000)
+                    throw new NoSuchElementException();
+            }
+
+            return driver.FindElements(by);
+        }
+
+        public static ReadOnlyCollection<IWebElement> FindElements(this IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            var durations = 0;
+
+            while (true)
+            {
+                try
+                {
+                    driver.FindElement(by);
+                    break;
+                }
+                catch (NoSuchElementException)
+                {
+                    durations += 100;
+                    Thread.Sleep(100);
+                }
+
+                if (durations > timeoutInSeconds * 1000)
+                    throw new NoSuchElementException();
+            }
+
+            return driver.FindElements(by);
+        }
+
+        public static void WaitingForDispocal(this IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            IWebElement result = null;
+            var durations = 0;
+
+            do
+            {
+                try
+                {
+                    result = driver.FindElement(by);
+                    durations += 100;
+                    Thread.Sleep(100);
+
+                }
+                catch (NoSuchElementException)
+                {
+                    break;
+                }
+
+                if (durations > timeoutInSeconds * 1000)
+                    throw new Exception();
+            }
+            while (result != null);
+        }
+    }
+}
---
+++ PhantomChromeDriver.cs
@@ -0,0 +1,79 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace SeleniumTools
+{
+    public class PhantomChromeDriver : ChromeDriver
+    {
+        public PhantomChromeDriver() : base(RemovingFlag()) { }
+        public PhantomChromeDriver(ChromeOptions options) : base(RemovingFlag(options)) { }
+
+        private static ChromeOptions RemovingFlag() => ModifyOptions(new ChromeOptions());
+        private static ChromeOptions RemovingFlag(ChromeOptions options) => ModifyOptions(options);
+
+        private static ChromeOptions ModifyOptions(ChromeOptions options)
+        {
+            options.AddExcludedArgument("enable-automation");
+            options.AddAdditionalChromeOption("useAutomationExtension", false);
+            options.AddAdditionalOption("useAutomationExtension", false);
+            options.AddArgument("--disable-infobars");
+            options.AddArgument("--disable-blink-features=AutomationControlled");
+
+            return options;
+        }
+    }
+
+    //public class Proxy
+    //{
+    //    IPAddress Ip { set; get; }
+    //    int Port { set; get; }
+    //    string Name { set; get; }
+    //    string Password { set; get; }
+
+    //    public static Proxy Parse(string proxyString)
+    //    {
+    //        var match = Regex.Match(proxyString, @"(\d+.\d+.\d+.\d+):(\d+):(.+):(.+)");
+
+    //        return new Proxy()
+    //        {
+    //            Ip = IPAddress.Parse(match.Groups[1].Value),
+    //            Port = int.Parse(match.Groups[2].Value),
+    //            Name = match.Groups[3].Value,
+    //            Password = match.Groups[4].Value
+    //        };
+    //    }
+    //}
+
+    public class WebDriverService
+    {
+        private PhantomChromeDriver _driver;
+        private string _profileName = null;
+
+        public WebDriverService()
+        {
+        }
+
+        public WebDriverService(string profileName)
+        {
+            _profileName = profileName;
+        }
+
+        public IWebDriver GetDriver()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--disable-notifications");
+            options.AddArgument("--dns-prefetch-disable");
+            options.AddArgument("--ignore-certificate-errors-spki-list");
+            options.AddArgument("--ignore-urlfetcher-cert-requests");
+            options.AddArgument("--ignore-certificate-errors");
+            options.AddArgument("--ignore-ssl-errors");
+            if (_profileName != null)
+                options.AddArgument($"--user-data-dir={Directory.GetCurrentDirectory()}/{_profileName}");
+            var driver = new PhantomChromeDriver(options);
+            driver.Manage().Window.Maximize();
+            return driver;
+        }
+    }
+}
